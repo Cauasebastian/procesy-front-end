068fcb983fb7b61d4db81b1f7be1138057905fb4
vieira <giullia.00000850245@unicap.br>
2024-12-11 23:59:47 -0300

    Feat: Adicionado funcionalidade de pesquisa e novos botões na página de Processos

--- src/pages/Processos/Processos.jsx
+++ src/pages/Processos/Processos.jsx
@@ -2,28 +2,95 @@ import processos from "../../mocks/processos.json"
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import IconButton from '@mui/material/IconButton';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import AddIcon from '@mui/icons-material/Add';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import styles from "./style.module.css"
-import { useNavigate } from 'react-router-dom';
-// adicionar imports para o header 
+import { Header } from '../../components/Header'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; 
 
 export default function Processos(){
     const navigate = useNavigate();
+    const [searchTerm, setSearchTerm] = useState('');
 
     function redirectToDetails(processo){
         navigate(`/processo/${processo.numero}`, { state: { processo }, replace: false})
     }
 
-    // adicionar função para filtrar os dados dos processos
-
+    // adicionar função para filtrar os dados dos processos om base no termo de pesquisa
+    const filteredProcessos = processos.filter(processo => 
+        processo.cliente.nome.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        processo.numero.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        processo.acao.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        processo.status.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        processo.codProcesso.toLowerCase().includes(searchTerm.toLowerCase())
+    ); 
     console.log(processos);
     return(
         <div className={styles.container}>
-            {/* adicionar o header */}
+            <Header />
+            <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", mb: 2, marginTop: 4}}>
+                    <TextField
+                        label="Cliente, número do processo, ação..."
+                        variant="outlined"
+                        value={searchTerm}
+                        onChange={(event) => setSearchTerm(event.target.value)}
+                        sx={{ 
+                            width: '40%', 
+                        '& .MuiOutlinedInput-root': {
+                            borderTopRightRadius: '0px',
+                            borderBottomRightRadius: '0px',
+                        }
+                    }}
+                    />
+                    <Button 
+                    variant="contained" 
+                    color="primary" 
+                    onClick={() => console.log('Pesquisar')}
+                    sx={{  
+                        ml: 0, 
+                        backgroundColor: '#454B60', 
+                        height: '56px', 
+                        borderTopLeftRadius: '0px',
+                        borderBottomLeftRadius: '0px'
+                    }} 
+                >
+                    Pesquisar
+                </Button>
+            </Box> 
+                <Box sx={{ display: 'center', justifyContent: 'center', mb: 4, width: '50%', margin: '0 auto' }}>
+                    <Button 
+                        variant="contained"
+                        sx={{ mr: 4, backgroundColor: '#454B60', minWidth: '52px', height: '52px' }}
+                        onClick={() => navigate('/novo-processo')}
+                        endIcon={<AddIcon />}
+                        padding="6px 26px"
+                    >
+                        Novo Processo
+                    </Button>
+                    <Button 
+                        variant="contained" 
+                        sx={{ mr: 4, backgroundColor: '#454B60', minWidth: '52px', height: '52px' }} 
+                        onClick={() => navigate('/novo-cliente')}
+                        endIcon={<PersonAddIcon />}
+                        padding="6px 26px"
+                    >
+                        Novo Cliente
+                    </Button>
+                    <IconButton 
+                        sx={{ backgroundColor: '#454B60', color: 'white', minWidth: '52px', height: '52px', borderRadius: '3px' }}
+                        onClick={() => console.log('Filtro')}
+                    >
+                        <FilterListIcon />
+                    </IconButton>
+                </Box>
 
-            {/* Fazer um box de Pesquisa */}
-            
             <h2 className={styles.title_page} onClick={() => console.log("olá")}>Processos</h2>
             <div className={styles.container_cards}>
                 {processos.map((processo) => (
