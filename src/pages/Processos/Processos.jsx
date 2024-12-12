9231e024a7dc537608569c2ee8a110dc70a753b6
vieira <giullia.00000850245@unicap.br>
2024-12-11 21:12:14 -0300

    Feat:  comentários futuras implementações na página de Processos

--- src/pages/Processos/Processos.jsx
+++ src/pages/Processos/Processos.jsx
@@ -6,18 +6,28 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Typography from '@mui/material/Typography';
 import styles from "./style.module.css"
 import { useNavigate } from 'react-router-dom';
+// adicionar imports para o header 
 
 export default function Processos(){
     const navigate = useNavigate();
+
     function redirectToDetails(processo){
         navigate(`/processo/${processo.numero}`, { state: { processo }, replace: false})
     }
+
+    // adicionar função para filtrar os dados dos processos
+
     console.log(processos);
     return(
         <div className={styles.container}>
+            {/* adicionar o header */}
+
+            {/* Fazer um box de Pesquisa */}
+            
             <h2 className={styles.title_page} onClick={() => console.log("olá")}>Processos</h2>
             <div className={styles.container_cards}>
                 {processos.map((processo) => (
+                    // ajustar o css dos cartões individuais de cada processo
                     <Card  
                         key={processo.numero} 
                         onClick={() => redirectToDetails(processo)}
@@ -30,6 +40,7 @@ export default function Processos(){
                             </IconButton>
                         }/>
                         <div className={styles.card_processo}>
+                            {/* Div para o número do processo */}
                             <div>
                                 <Typography className={styles.title_atribute}>
                                     Número
@@ -38,6 +49,7 @@ export default function Processos(){
                                     {processo.numero}
                                 </Typography>
                             </div>
+                            {/* Div para a data de status */}
                             <div>
                                 <Typography className={styles.title_atribute}>
                                     Cliente
@@ -46,6 +58,7 @@ export default function Processos(){
                                     {processo.cliente.nome}
                                 </Typography>
                             </div>
+                           {/* Div para o código do processo */}
                             <div>
                                 <Typography className={styles.title_atribute}>
                                     Data Status
@@ -54,6 +67,7 @@ export default function Processos(){
                                     {processo.dataStatus}
                                 </Typography>
                             </div>
+                            {/* Div para o código do processo */}
                             <div>
                                 <Typography className={styles.title_atribute}>
                                     N° Processo
@@ -62,6 +76,7 @@ export default function Processos(){
                                     {processo.codProcesso}
                                 </Typography>
                             </div>
+                            {/* Div para a ação */}
                             <div>
                                 <Typography className={styles.title_atribute}>
                                     Ação
@@ -70,6 +85,7 @@ export default function Processos(){
                                     {processo.acao}
                                 </Typography>
                             </div>
+                            {/* Div para o status */}
                             <div>
                                 <Typography className={styles.title_atribute}>
                                     Status
